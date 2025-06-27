dbadfc319804d963e85dfd8ff5b74090a8ad0952
Dan Maxim <dmaxim@mxinformatics.com>
2025-06-27 14:05:32 -0400

    Add PMC ID and DOI

--- src/BioAnalyzer/BioAnalyzer.Research.Api/Domain/Models/EntrezSummaryResponse.cs
+++ src/BioAnalyzer/BioAnalyzer.Research.Api/Domain/Models/EntrezSummaryResponse.cs
@@ -7,9 +7,13 @@ public class EntrezSummaryResponse
     // Parse DocSum nodes from the XML document extracting the Id element value and the Item with a Name of "Title".
     public EntrezSummaryResponse(XmlDocument xmlDoc)
     {
-        //if (xmlDoc == null) return;
-
-        foreach (XmlNode docSumNode in xmlDoc.SelectNodes("//DocSum"))
+        var documents = xmlDoc.SelectNodes("//DocSum");
+        if (documents == null)
+        {
+            return;
+        }
+        
+        foreach (XmlNode docSumNode in documents)
         {
             var result = new EntrezSummaryResult();
             var uidNode = docSumNode.SelectSingleNode("Id");
@@ -23,6 +27,18 @@ public class EntrezSummaryResponse
             if (titleNode != null)
             {
                 result.Title = titleNode.InnerText;
+            }
+            
+            var articleIds = docSumNode.SelectSingleNode("Item[@Name='ArticleIds']");
+            var pmcIdNode = articleIds?.SelectSingleNode("Item[@Name='pmc']");
+            if (pmcIdNode != null)
+            {
+                result.SetPmcId(pmcIdNode.InnerText);
+            }
+            var doiNode = articleIds?.SelectSingleNode("Item[@Name='doi']");
+            if (doiNode != null)
+            {
+                result.Doi = doiNode.InnerText;
             }
             Results.Add(result);
         }
--- src/BioAnalyzer/BioAnalyzer.Research.Api/Domain/Models/EntrezSummaryResult.cs
+++ src/BioAnalyzer/BioAnalyzer.Research.Api/Domain/Models/EntrezSummaryResult.cs
@@ -4,4 +4,12 @@ public class EntrezSummaryResult
 {
     public string Uid { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
+    
+    public string PmcId { get; set; } = string.Empty;
+    
+    public string Doi { get; set; } = string.Empty;
+    public void SetPmcId(string pmcId)
+    {
+       PmcId = pmcId.Replace("pmc", "");
+    }
 }
