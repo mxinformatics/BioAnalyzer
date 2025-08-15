e73ceecc3a4e947e9672ccec5262c6b5c37169d2
Dan Maxim <dmaxim@mxinformatics.com>
2025-08-15 09:30:56 -0400

    Add Docker files

--- src/BioAnalyzer/BioAnalyzer.AppHost/Program.cs
+++ src/BioAnalyzer/BioAnalyzer.AppHost/Program.cs
@@ -1,8 +1,18 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+//
+// var sql = builder.AddSqlServer("bioAnalyzerSql")
+//     .WithVolume("bioAnalyzerSqlData", "/var/opt/mssql");
+//    //.WithLifetime(ContainerLifetime.Persistent);
+//
+// var db = sql
+//     .AddDatabase("BioAnalyzer");
+
 var researchApi = builder
     .AddProject<Projects.BioAnalyzer_Research_Api>("researchApi")
     //.WithHttpEndpoint(env: "RESEARCH_API_PORT")
+   // .WaitFor(db)
+   // .WithReference(db)
     .WithExternalHttpEndpoints();
 
 builder.AddProject<Projects.BioAnalyzer_App>("researchApp")
@@ -11,6 +21,9 @@ builder.AddProject<Projects.BioAnalyzer_App>("researchApp")
     //.WithHttpEndpoint(env: "RESEARCH_APP_PORT")
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
+
+
+    
 
 builder.AddAzureFunctionsProject<Projects.BioAnalyzer_EventHandlers>("eventHandlers")
     .PublishAsDockerFile();
